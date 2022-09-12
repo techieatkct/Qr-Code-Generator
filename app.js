1493b34f2ba46d9ef0a6e5899e68f53d7f856c8b
techieatkct <108172869+techieatkct@users.noreply.github.com>
2022-09-12 19:54:11 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var qrcode = new QRCode(document.querySelector(".qrcode"));
+qrcode.makeCode("Why did you scanned me?");
+function generateQr() {
+  qrcode.makeCode(document.querySelector("input").value);
+}
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
